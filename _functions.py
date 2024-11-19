319a00c371f2340dd852236db5f312210a136902
Bence Takacs <dtn125@alumni.ku.dk>
2024-11-19 09:21:22 -0500

    Added differential correction algorithm function, but do to  partial derivatives i can't really make it a function, so it'l have to be its own kind of algorithmic file

--- _functions.py
+++ _functions.py
@@ -10,8 +10,6 @@ def coordTransformMatrix(
     if units == 'd':
         lat *= np.pi / 180
         lon *= np.pi / 180
-    else:
-        pass
 
     m = np.array([
         [np.sin(lat) * np.cos(lon),  -np.sin(lon),  np.cos(lat) * np.cos(lon)],
@@ -22,3 +20,34 @@ def coordTransformMatrix(
     m = m.T if to_ijk == False else m
 
     return m
+
+###############################################################################
+
+def differentialCorrectionAlgorithm(
+        data: list,
+        initial_estimate: list,
+        parametric_equation,
+        partial_differential_matrix,
+        ):
+    """
+    Doesn't work for now
+    """
+    # Correct formats:
+    data = np.array(data).T
+    initial_estimate = np.array(initial_estimate).T
+
+    # Separate data into x and y
+    x = np.array(data[:, 0]).T
+    y = np.array(data[:, 1]).T
+
+    # Calculate initial residuals
+    ybar = np.array(parametric_equation(x))
+    btild = y - ybar
+
+    # Matrix Algebra
+    ata = np.matmul(partial_differential_matrix.T, partial_differential_matrix)
+    ata_inv = np.linalg.inv(ata)
+    atb = np.matmul(partial_differential_matrix.T, btild)
+    deltaz = np.matmul(ata_inv, atb)
+
+    new_estimate = initial_estimate + deltaz

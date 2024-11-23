35dbfd74fa54ce490afb004ba6f512a73100aad2
Bence Takacs <dtn125@alumni.ku.dk>
2024-11-22 23:51:36 -0500

    updated orbital elemetns from 3 position vectors function, renaming it orbitalElemGibbs

--- _functions.py
+++ _functions.py
@@ -93,8 +93,9 @@ def magnitude(r: list):
 
 ###############################################################################
 
-def orbitDetermThreePosVec(
-        r1: list, r2: list, r3: list, units: str = 'CANONUNITS'
+def orbitalElemGibbs(
+        r1: list, r2: list, r3: list, units: str = 'CANONUNITS',
+        angle_units: str = 'DEG'
 ):
     """
     Function which determines orbital parameters given 3 position vectors.
@@ -109,6 +110,8 @@ def orbitDetermThreePosVec(
         3rd observation
     units : str
         Units of position vectors. Options are 'CANONUNITS', 'NM', or 'KM'.
+    angle_units : str
+        Units of inclination angle. Options are 'RAD', 'DEG'.
 
     Returns
     -------
@@ -116,18 +119,21 @@ def orbitDetermThreePosVec(
     """
 
     # Check position vectors
-    if r1.shape != (1, 3) or r2.shape != (1, 3) or r3.shape != (1, 3): 
-        raise ValueError('Position vectors must be of shape (1, 3).')
-    
     if (
         (type(r1) != np.ndarray) or (type(r2) != np.ndarray) or              (type(r3) != np.ndarray)
     ):
         r1 = np.array(r1); r2 = np.array(r2); r3 = np.array(r3)
+    if r1.shape != (1, 3) or r2.shape != (1, 3) or r3.shape != (1, 3): 
+        raise ValueError('Position vectors must be of shape (1, 3).')
+    
     
     # Test that position vectors are coplanar:
     coplan_test = np.dot(r1, (np.cross(r2, r3)).T)
-    if coplan_test != 0:
+    if coplan_test > 1e-6:  # add a little bit of tolerance
         raise ValueError('Position vectors must be coplanar.')
+    
+    # Calculate in rads or degs
+    angle = 1 if angle_units == 'RAD' else (180 / np.pi)
 
     # Calculate vector magnitudes
     r1m = magnitude(r1)
@@ -140,9 +146,13 @@ def orbitDetermThreePosVec(
     s = (r2m - r3m) * r1 + (r3m - r1m) * r2 + (r1m - r2m) * r3
 
     # Test D != 0, N != 0, and D dot N > 0 to assure vectors decsribe possible two-body orbit
+    print_statement = True
     dtest = magnitude(d); ntest = magnitude(n); dntest = np.dot(d, n.T)
     if dtest == 0 or ntest == 0 or dntest <= 0:
-        raise ValueError('Vectors do not describe possible two-body orbit.')
+        # raise ValueError('Vectors do not describe possible two-body orbit.')
+        print_statement = False
+        print('WARNING: Vectors do not describe possible two-body orbit.')
+        
     
     # Calculate B = D cross r
     b1 = np.cross(d, r1); b2 = np.cross(d, r2); b3 = np.cross(d, r3)
@@ -163,15 +173,19 @@ def orbitDetermThreePosVec(
     v1m = magnitude(v1); v2m = magnitude(v2); v3m = magnitude(v3)
 
     # Calculate orbital parameters
-    p = ntest / dtest# semi latus rectum, L
     e = magnitude(s) / dtest # eccentricity
+    p = ntest / dtest# semi latus rectum, L
+    h = np.cross(r1, v1)
     hm = np.sqrt(ntest * mu / dtest) # angular momentum magnitude L^2/T^2
-    a = p / (1 - e**2) # semi major axis, L
+    inc = np.arccos(h[0][0] / hm) * angle # inclination, degrees
+    a = np.inf if e == 1 else p / (1 - e**2) # semi major axis, L
     tperiod = 2 * np.pi / np.sqrt(mu) * a**(3/2) # period, S
 
     # Calculate perifocal basis vectors in IJK
-    qvec = s / magnitude(s)
-    wvec = n / magnitude(n)
+    # Test for zero vectors
+    zero_vec = np.zeros((1, 3))
+    qvec = zero_vec if (s == zero_vec).all() else s / magnitude(s)
+    wvec = zero_vec if (n == zero_vec).all() else n / magnitude(n)
     pvec = np.cross(qvec, wvec)
 
     # Convert r, v, pvec, qvec, wvec to strings and round to 2 places for printing
@@ -189,55 +203,56 @@ def orbitDetermThreePosVec(
         runit = 'n.mi.'; vunit = 'n.mi./s'; punit = 'n.mi.'; aunit = 'n.mi.'; tunit = 'hours'; tperiod *= sec_hour
 
     w = 5 if units == 'CANONUNITS' else 9
-    print(
-        '\nOrbital Parameters given the position vectors:\n'
-        '  r1 = [{}]  ;  r2 = [{}]  ;  r3 = [{}] {}\n'
-        '       |{}|          |{}|          |{}|\n'
-        '       [{}]          [{}]          [{}]\n'
-        '|r1| =  {}    |r2| =  {}    |r3| = {}\n\n'
-        'Perifocal basis vectors in IJK system:\n'
-        '   P = [{}]  ;   Q = [{}]  ;   W = [{}]\n'
-        '       |{}|          |{}|          |{}|\n'
-        '       [{}]          [{}]          [{}]\n\n'
-        'Velocity vectors at each position:\n'
-        '  v1 = [{}]  ;  v2 = [{}]  ;  v3 = [{}] {}\n'
-        '       |{}|          |{}|          |{}|\n'
-        '       [{}]          [{}]          [{}]\n'
-        '|v1| =  {}    |v2| =  {}    |v3| = {}\n\n'
-        'Orbital elements:\n'
-        'semi-latus pectum p = {:.2f} {} ; eccentricity e = {:.2f}\n'
-        'semi-major axis   a = {:.2f} {} ; period       T = {:.2f} {}'
-        ''.format(
-            # r1, r2, and r3 and their units and magnitudes
-            str.center(r1s[0][0], w), str.center(r2s[0][0], w),
-            str.center(r3s[0][0], w), runit,
-            str.center(r1s[0][1], w),
-            str.center(r2s[0][1], w), str.center(r3s[0][1], w),
-            str.center(r1s[0][2], w), str.center(r2s[0][2], w),
-            str.center(r3s[0][2], w),
-            str.center(str(np.round(r1m, 2)), w),
-            str.center(str(np.round(r2m, 2)), w),
-            str.center(str(np.round(r3m, 2)), w),
-            # perifocal basis vectors p, q, w
-            str.center(ps[0][0], w), str.center(qs[0][0], w),
-            str.center(ws[0][0], w), str.center(ps[0][1], w),
-            str.center(qs[0][1], w), str.center(ws[0][1], w),
-            str.center(ps[0][2], w), str.center(qs[0][2], w),
-            str.center(ws[0][2], w),
-            # v1, v2, v3
-            str.center(v1s[0][0], w), str.center(v2s[0][0], w),
-            str.center(v3s[0][0], w), vunit,
-            str.center(v1s[0][1], w),
-            str.center(v2s[0][1], w), str.center(v3s[0][1], w),
-            str.center(v1s[0][2], w), str.center(v2s[0][2], w),
-            str.center(v3s[0][2], w),
-            str.center(str(np.round(v1m, 2)), w),
-            str.center(str(np.round(v2m, 2)), w),
-            str.center(str(np.round(v3m, 2)), w),
-            # orbital elements and their units
-            p, punit, e,
-            a, aunit, tperiod, tunit
+    if print_statement:
+        print(
+            '\nOrbital Parameters given the position vectors:\n'
+            '  r1 = [{}]  ;  r2 = [{}]  ;  r3 = [{}] {}\n'
+            '       |{}|          |{}|          |{}|\n'
+            '       [{}]          [{}]          [{}]\n'
+            '|r1| =  {}    |r2| =  {}    |r3| = {}\n\n'
+            'Perifocal basis vectors in IJK system:\n'
+            '   P = [{}]  ;   Q = [{}]  ;   W = [{}]\n'
+            '       |{}|          |{}|          |{}|\n'
+            '       [{}]          [{}]          [{}]\n\n'
+            'Velocity vectors at each position:\n'
+            '  v1 = [{}]  ;  v2 = [{}]  ;  v3 = [{}] {}\n'
+            '       |{}|          |{}|          |{}|\n'
+            '       [{}]          [{}]          [{}]\n'
+            '|v1| =  {}    |v2| =  {}    |v3| = {}\n\n'
+            'Orbital elements:\n'
+            'semi-latus pectum p = {:.2f} {} ; eccentricity e = {:.2f}\n'
+            'semi-major axis   a = {:.2f} {} ; period       T = {:.2f} {}\n'
+            'inclination       i = {:.2f} {}'
+            ''.format(
+                # r1, r2, and r3 and their units and magnitudes
+                str.center(r1s[0][0], w), str.center(r2s[0][0], w),
+                str.center(r3s[0][0], w), runit,
+                str.center(r1s[0][1], w),
+                str.center(r2s[0][1], w), str.center(r3s[0][1], w),
+                str.center(r1s[0][2], w), str.center(r2s[0][2], w),
+                str.center(r3s[0][2], w),
+                str.center(str(np.round(r1m, 2)), w),
+                str.center(str(np.round(r2m, 2)), w),
+                str.center(str(np.round(r3m, 2)), w),
+                # perifocal basis vectors p, q, w
+                str.center(ps[0][0], w), str.center(qs[0][0], w),
+                str.center(ws[0][0], w), str.center(ps[0][1], w),
+                str.center(qs[0][1], w), str.center(ws[0][1], w),
+                str.center(ps[0][2], w), str.center(qs[0][2], w),
+                str.center(ws[0][2], w),
+                # v1, v2, v3
+                str.center(v1s[0][0], w), str.center(v2s[0][0], w),
+                str.center(v3s[0][0], w), vunit,
+                str.center(v1s[0][1], w),
+                str.center(v2s[0][1], w), str.center(v3s[0][1], w),
+                str.center(v1s[0][2], w), str.center(v2s[0][2], w),
+                str.center(v3s[0][2], w),
+                str.center(str(np.round(v1m, 2)), w),
+                str.center(str(np.round(v2m, 2)), w),
+                str.center(str(np.round(v3m, 2)), w),
+                # orbital elements and their units
+                p, punit, e,
+                a, aunit, tperiod, tunit,
+                inc, angle_units
+            )
         )
-    )
-
-###############################################################################

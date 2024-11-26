7440f5fbf76579f1501dad7b33e2b8dda1181fa1
Bence Takacs <dtn125@alumni.ku.dk>
2024-11-25 21:52:38 -0500

    added kepler problem algorithm, still need to generalize to include hyperbolic

--- _functions.py
+++ _functions.py
@@ -261,3 +261,107 @@ def orbitalElemGibbs(
 
 def sign(x):
     return '+' if x >= 0 else '-'
+
+###############################################################################
+
+def kepler_problem(
+        r0: list, v0: list, t: float, t0: float = 0.0, units: str = 'CANON'
+):
+    """
+    Solves Kepler's problem for a given initial position vector, velocity vector, time at which to find r and v.
+
+    Parameters:
+    r0 (list): Initial position vector [x, y, z] in the IJK system.
+    v0 (list): Initial velocity vector [vx, vy, vz] in the IJK system.
+    t (float): Time at which to find r and v.
+    t0 (float): Initial time. Default is 0.0.
+    units (str): Unit system.
+
+    Returns:
+    r (list): Position vector [x, y, z] in the IJK system at time t.
+    v (list): Velocity vector [vx, vy, vz] in the IJK system at time t.
+    """
+
+    # Make r0 and v0 into np.array
+    if type(r0) != np.ndarray or type(v0) != np.ndarray:
+        r0 = np.array(r0)
+        v0 = np.array(v0)
+
+    # Get correct units for m, and create sqrt(m) var
+    mu = 1 if units == 'CANON' else mu_nmi_s if units == 'NMI' else mu_ft_s
+    mur = np.sqrt(mu)
+
+    # Derive r0, v0 magnitudes
+    r0m = magnitude(r0)
+    v0m = magnitude(v0)
+
+    # Step 1: Fro`m r0, v0, determine r0m and a -------------------------------
+
+    # Orbital constants
+    h = np.dot(r0, v0.T) # angular momentum, DU^2/TU
+    p = h**2 / mu # orbital parameter, DU
+    sme =v0m**2 / 2 - mu / r0m # specific mechanical energy, DU^2/TU^2
+    a = - mu / (2 * sme) # semi-major axis, DU
+
+    # Step 2: Given (t - t0), solve universal time-of-flight (TOF) equation for x using Newton iteration scheme -------------------------------------------------
+
+    # Define C(z), S(z)
+    C = lambda z: (1 - np.cos(np.sqrt(z))) / 2
+    S = lambda z: (np.sqrt(z) - np.sin(np.sqrt(z))) / np.sqrt(z**3)
+
+    # Newton Iteration Scheme -----------------------------------------------------
+
+    # First guess for x:
+    xn = mur * (t - t0) / a
+
+    # Define variable z
+    z = lambda x: x**2 / a
+
+    # Define differential equaiton dt/dx
+    dtdx = lambda x: x**2 * C(z(x)) / mur + h / mur * (1 - z(x) * S(z(x)) + r0m / mur * (1 - z(x) * C(z(x))))
+
+    # Define t_n equation
+    tn = lambda xn: h / mu * xn**2 * C(z(xn)) + (1 - r0m/a) * xn**3 * S(z(xn)) / mur + r0m * xn / mur
+
+    # Iterate until convergence (t ~ tn)
+    for n in range(10):
+        tn1 = tn(xn)
+        xn1 = xn + (t - tn1) / dtdx(xn)
+
+        tn2 = tn(xn1)
+
+        if np.abs(t - tn2) < 1e-4:
+            break
+        else:
+            tn1 = tn2
+            xn = xn1
+
+    # Universal variable, x
+    x = xn1
+
+    # Step 3: Evaluate f & g then compute r ---------------------------------------
+    f = 1 - (a / r0m) * (1 - np.cos(x / np.sqrt(a)))
+    g = t - x**3 / mur * S(z(x))
+
+    # Compute r using f & g
+    r = f * r0 + g * v0
+    rm = magnitude(r)
+
+    # Step 4: Evaluate fdot & gdot then compute v ---------------------------------
+    fdot = - np.sqrt(mu * a) / r0m / rm * np.sin(x / np.sqrt(a))
+    gdot = 1 - a / rm + a / rm * np.cos(x / np.sqrt(a))
+
+    # Compute v using fdot & gdot
+    v = fdot * r0 + gdot * v0
+    vm = magnitude(v)
+
+    # Output results
+    w = 2 # number of places to round to
+    print('\n'
+        'Final Position Vector r(t={}) = {}{} I {} {} J {} {} K\n'
+        'Final Velocity Vector v(t={}) = {}{} I {} {} J {} {} K\n'
+        ''.format(
+            t, sign(r[0]), np.round(np.abs(r[0]), w), sign(r[1]), np.round(np.abs(r[1]), w), sign(r[2]), np.round(np.abs(r[2]), w),
+            t, sign(v[0]), np.round(np.abs(v[0]), w), sign(v[1]), np.round(np.abs(v[1]), w), sign(v[2]), np.round(np.abs(v[2]), w),
+        ))
+
